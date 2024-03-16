90b8ad26cd3ded072d76401f6949573737a1aa2e
catalina-to24 <catalinatempraotero@gmail.com>
2024-03-15 23:29:51 -0300

    errores de las tablas

--- src/main/java/com/dh/TPIdiego_cata/entity/Paciente.java
+++ src/main/java/com/dh/TPIdiego_cata/entity/Paciente.java
@@ -22,51 +22,5 @@ public class Paciente {
     @OneToMany(mappedBy = "paciente")
     Set<Turno> turnos;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getDni() {
-        return dni;
-    }
-
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public LocalDate getFechaIngreso() {
-        return fechaIngreso;
-    }
-
-    public void setFechaIngreso(LocalDate fechaIngreso) {
-        this.fechaIngreso = fechaIngreso;
-    }
-
-    public Domicilio getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(Domicilio domicilio) {
-        this.domicilio = domicilio;
-    }
 }
